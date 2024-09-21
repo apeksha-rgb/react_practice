b64a1832f24af44000192aa483e9d9059b38c5c2
apeksha-rgb <apeksha.manshi.shakya@gmail.com>
2024-09-21 15:51:54 +0530

    learnt making custom hooks & tailwind

--- 06profileCreater/src/App.jsx
+++ 06profileCreater/src/App.jsx
@@ -1,17 +1,25 @@
-
+import { useState } from 'react'
 import './App.css'
 
 const user = {
   username: "Apeksha Shakya",
-  imageUrl:"https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60",
-  imageSize: 70
+  imageUrl:"/images/main_image.png",
+  imageSize: 300
 }
 
 function MyButton(){
+  const [count, setCount] = useState(0)
+  function handleClick(){
+    setCount(count + 1)
+  
+  }
   return(
     <>
-    <button>
-      Click to know more
+    <button onClick={handleClick} count ={count}>
+      Clicked {count} times
+    </button>
+    <button onClick={handleClick} count ={count}>
+      Clicked {count} times
     </button>
     </>
   )
