b318967e87b480ab4499c2fa1aac1136282c241d
apeksha-rgb <apeksha.manshi.shakya@gmail.com>
2025-04-02 13:45:36 +0530

    added post-form

--- 14megaBlog/src/components/index.js
+++ 14megaBlog/src/components/index.js
@@ -3,12 +3,14 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn"
+import RTE from "./RTE";
 
 export {
     Header,
     Footer,
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
 
 }
